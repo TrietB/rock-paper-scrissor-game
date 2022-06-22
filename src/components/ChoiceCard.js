075311d79306643fe373e1247f809c2867e00bae
TrietB <triet.bm1996@gmail.com>
2022-06-22 18:29:30 +0700

    done

--- src/components/ChoiceCard.js
+++ src/components/ChoiceCard.js
@@ -4,9 +4,7 @@ function ChoiceCard({ gameItem, handleGameItemChange }) {
   const { url, name } = gameItem;
   return (
     <img
-      onClick={() => {
-        "Your code here";
-      }}
+      onClick={() => {{handleGameItemChange(gameItem)}}}
       className="choice"
       src={url}
       alt={name}
